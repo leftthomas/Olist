320742277a974e53362a3865c422548785ed2b14
leftthomas <leftthomas@qq.com>
2018-11-03 15:24:27 +0800

    add clear data

--- data_clear.py
+++ data_clear.py
@@ -1,36 +1,23 @@
 import pandas as pd
 
-product_names_table = pd.read_csv('data/product_category_name_translation.csv')
-customers_table = pd.read_csv('data/olist_public_dataset_v2_customers.csv')
-products_table = pd.read_csv('data/product_measures_olist_public_dataset_.csv')
-sellers_table = pd.read_csv('data/sellers_olist_public_dataset_.csv')
-payment_table = pd.read_csv('data/payments_olist_public_dataset.csv')
-geolocation_table = pd.read_csv('data/geolocation_olist_public_dataset.csv')
-unclassified_orders = pd.read_csv('data/olist_public_dataset_v2.csv')
-classified_orders = pd.read_csv('data/olist_classified_public_dataset.csv')
+customers = pd.read_csv('data/olist_customers_dataset.csv')
+geolocations = pd.read_csv('data/olist_geolocation_dataset.csv')
+items = pd.read_csv('data/olist_order_items_dataset.csv')
+payments = pd.read_csv('data/olist_order_payments_dataset.csv')
+reviews = pd.read_csv('data/olist_order_reviews_dataset.csv')
+orders = pd.read_csv('data/olist_orders_dataset.csv')
+products = pd.read_csv('data/olist_products_dataset.csv')
+sellers = pd.read_csv('data/olist_sellers_dataset.csv')
+product_names_translations = pd.read_csv('data/product_category_name_translation.csv')
 
-
-# merge translations for category names
-unclassified_orders = unclassified_orders.merge(product_names_table, on='product_category_name').drop(
-    'product_category_name', axis=1)
-unclassified_orders.rename(columns={'product_category_name_english': 'product_category_name'}, inplace=True)
-classified_orders = classified_orders.merge(product_names_table, on='product_category_name').drop(
-    'product_category_name', axis=1)
-classified_orders.rename(columns={'product_category_name_english': 'product_category_name'}, inplace=True)
-
-# merge customer ids
-unclassified_orders = unclassified_orders.merge(customers_table, on='customer_id').drop('customer_id', axis=1)
-unclassified_orders.rename(columns={'customer_unique_id': 'customer_id'}, inplace=True)
-
-# drop the first two columns
-classified_orders.drop(['Unnamed: 0', 'id'], axis=1, inplace=True)
-# drop the review_comment_title column, because all values are null
-classified_orders.drop(['review_comment_title'], axis=1, inplace=True)
-# drop the review_id column, because this value is same as order_id
-unclassified_orders.drop(['review_id'], axis=1, inplace=True)
-# drop the duplicate rows
-classified_orders.drop_duplicates(inplace=True)
-
-# payment_table.dropna(inplace=True)
-# payment_table.drop_duplicates(inplace=True)
-# df.to_csv('data/classified_orders.csv', index=False)
+customers.to_csv('data/customers.csv')
+geolocations.to_csv('data/geolocations.csv')
+items.to_csv('data/items.csv')
+payments.to_csv('data/payments.csv')
+reviews.to_csv('data/reviews.csv')
+orders.dropna(inplace=True)
+orders.to_csv('data/orders.csv')
+products.dropna(inplace=True)
+products.to_csv('data/products.csv')
+sellers.to_csv('data/sellers.csv')
+product_names_translations.to_csv('data/translations.csv')
