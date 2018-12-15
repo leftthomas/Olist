73fefff5e7f77646f5979190adc6fb0854a0356a
leftthomas <leftthomas@qq.com>
2018-12-16 00:41:53 +0800

    finish main layout

--- app.py
+++ app.py
@@ -13,6 +13,26 @@ def dashboard():
     return render_template('dashboard.html')
 
 
+@app.route('/products')
+def products():
+    return render_template('products.html')
+
+
+@app.route('/orders')
+def orders():
+    return render_template('orders.html')
+
+
+@app.route('/reviews')
+def reviews():
+    return render_template('reviews.html')
+
+
+@app.route('/maps')
+def maps():
+    return render_template('maps.html')
+
+
 # @app.route('/hello/<name>')
 # def show_user_profile(name):
 #     return render_template('dashboard.html', name=name)
